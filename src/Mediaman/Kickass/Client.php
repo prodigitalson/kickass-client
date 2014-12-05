ea3f7e01887ea8c6a4715335241980df7d358fc2
Ant Cunningham <ant.cunningham@gmail.com>
2014-12-05 15:58:47 -0500

    Adding composer.json and class skeleton.

---
+++ src/Mediaman/Kickass/Client.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mediaman\Kickass\Client;
+
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Client extends \Goutte\Client
+{
+  
+}
